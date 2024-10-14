faaa6766b4de309dce08e56d1dfc8a88cdbd6341
nowlabstudio <167752979+nowlabstudio@users.noreply.github.com>
2024-10-14 12:29:14 +0200

    Create axis_repo.ino

---
+++ axis_repo.ino
@@ -0,0 +1,11 @@
+#include "Arduino.h"
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Setup complete.");
+}
+
+void loop() {
+    Serial.println("Loop running...");
+    delay(1000);
+}
